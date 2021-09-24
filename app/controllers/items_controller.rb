01dfe9ca1c913cf6266a7e169ecf08c026ceb774
taiyakio <itaiki1998@gmail.com>
2021-09-24 14:56:32 +0900

    商品保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
     @items = Item.all
   end
@@ -19,6 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :category_id, :condision_id, :shippingcharges_id, :area_id, :shippindate_id, :image).merge(user_id: current_user.id)
   end
 
 end
