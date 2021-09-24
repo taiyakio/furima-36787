0cc76f7bc5b07099303971edec9843c8a39fb1da
taiyakio <itaiki1998@gmail.com>
2021-09-24 15:47:51 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, :price, :explanation, :category_id, :condision_id,
+            :shippingcharges_id, :area_id, :shippingdate_id, :image, presence: true
 end
