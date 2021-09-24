a1e78e311290a30b43c2b4fc1301ee6da03a623e
taiyakio <itaiki1998@gmail.com>
2021-09-24 18:33:06 +0900

    誤字の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :price, :explanation, :category_id, :condision_id, :shippingcharges_id, :area_id, :shippingdate_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :explanation, :category_id, :condition_id, :shippingcharges_id, :area_id, :shippingdate_id, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
