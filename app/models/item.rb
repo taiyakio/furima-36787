08e5ebfd67a776933c3c134d11a03263dfbd2501
taiyakio <itaiki1998@gmail.com>
2021-09-24 18:32:54 +0900

    Itemモデルのアソシエーションの設定2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :price, :explanation, :category_id, :condision_id,
+  validates :name, :price, :explanation, :category_id, :condition_id,
             :shippingcharges_id, :area_id, :shippingdate_id, :image, presence: true
 end
