7b27864fbf35c5e1b6cbe8d56d818b1fd78c2459
taiyakio <itaiki1998@gmail.com>
2021-09-24 16:14:47 +0900

    ログアウト時のリダイレクト機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,4 +31,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :explanation, :category_id, :condision_id, :shippingcharges_id, :area_id, :shippingdate_id, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
