ab9fc83c29545bc8db01cffc1fb927f769eab758
taiyakio <itaiki1998@gmail.com>
2021-09-24 18:40:13 +0900

    Itemモデルのアソシエーションの設定3

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shippingcharges
   belongs_to :user
   has_one_attached :image
 
