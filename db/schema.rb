a6de964fb818c568e3ea5f175689678a91d1c7c9
taiyakio <itaiki1998@gmail.com>
2021-09-24 15:36:36 +0900

    itemテーブルにexplanationカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_051828) do
+ActiveRecord::Schema.define(version: 2021_09_24_063310) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_051828) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "explanation"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
