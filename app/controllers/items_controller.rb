048bc660e6bdd6346e380a3279b0be517ba88034
taiyakio <itaiki1998@gmail.com>
2021-09-25 11:09:30 +0900

    保存できないときにエラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,12 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
-    redirect_to "/"
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
